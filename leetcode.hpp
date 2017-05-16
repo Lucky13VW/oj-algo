9e2144e88c3bc9e3dae8692a2c2725938738869f
lucky13vw <vw01eipi@outlook.com>
2017-05-16 18:08:31 +0800

    LRUCache

--- leetcode.hpp
+++ leetcode.hpp
@@ -1685,6 +1685,75 @@ public:
 };
 
 /*
+146. LRU Cache
+Design and implement a data structure for Least Recently Used (LRU) cache. 
+It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. 
+
+When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+e.g:
+LRUCache cache = new LRUCache( 2 );
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+class LRUCache {
+    struct Node {
+        int key;
+        int value;
+        Node(int k, int v) :
+            key(k), value(v) {}
+    };
+
+public:
+    LRUCache(int capacity)
+        :Capacity_(capacity)
+    {}
+
+    int get(int key) {
+        auto it = Index_.find(key);
+        if (it == Index_.end()) return -1;
+        // move the hit node to begin position
+        Cache_.splice(Cache_.begin(), Cache_, it->second);
+        return it->second->value;
+    }
+
+    void put(int key, int value) {
+        auto map_it = Index_.find(key);
+        if (map_it != Index_.end()) { 
+            // update value and move it to begin position
+            map_it->second->value = value;
+            Cache_.splice(Cache_.begin(), Cache_, map_it->second);
+        } else {
+            // remove the LRU node from both list and hashtable
+            if (Index_.size() == Capacity_)
+            {
+                int k = Cache_.back().key;
+                Cache_.pop_back();
+                Index_.erase(Index_.find(k));
+            }
+            Cache_.push_front(Node(key, value));
+            auto list_it = Cache_.begin();
+            Index_.insert(make_pair(key, list_it));
+        }
+    }
+
+private:
+    int Capacity_;
+    unordered_map<int, list<Node>::iterator> Index_;
+    list<Node> Cache_;
+};
+
+/*
 160. Intersection of Two Linked Lists
 */
 class IntersectionTwoLinkedLists 
